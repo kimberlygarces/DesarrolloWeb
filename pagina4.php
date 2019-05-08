53a0fea027958398fc00f93bccf8ad4b3e6b8368
Kimberly Garcés <kimberlygarces1994@gmail.com>
2019-05-07 19:58:47 -0500

    cambiando

--- pagina4.php
+++ pagina4.php
@@ -58,7 +58,13 @@ ul {
       <li class="nav"><a href="pagina1.php">PAGINA 1</a></li>
       <li class="nav"><a href="pagina2.php">PAGINA 2</a></li>
       <li class="nav"><a href="pagina3.php">PAGIANA 3</a></li>
-        <li class="nav"><a href="pagina4.php">PAGIANA 4</a></li>
+      <li class="nav"><a href="pagina4.php">PAGIANA 4</a></li>
+      <li class="nav"><a href="pagina5.php">PAGINA 5</a></li>
+      <li class="nav"><a href="pagina6.php">PAGIANA 6</a></li>
+      <li class="nav"><a href="pagina7.php">PAGIANA 7</a></li>
+      <li class="nav"><a href="pagina8.php">PAGINA 8</a></li>
+      <li class="nav"><a href="pagina9.php">PAGIANA 9</a></li>
+      <li class="nav"><a href="pagina10.php">PAGIANA 10</a></li>
     </ul>
 </div>
 </nav>
